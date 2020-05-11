fc59a8a0eca1a8bd50362b2f0cb1f3595da11139
LzDarnGamer <48888484+LzDarnGamer@users.noreply.github.com>
2020-05-11 19:42:59 +0100

    Achivements

---
+++ Assets/Resources/Scripts/AchivementList.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AchivementList : MonoBehaviour
+{
+    [SerializeField]
+    Sprite ach0;
+    [SerializeField]
+    Sprite ach1;
+    [SerializeField]
+    Sprite ach2;
+    [SerializeField]
+    Sprite ach3;
+    [SerializeField]
+    Sprite ach4;
+    [SerializeField]
+    Sprite ach5;
+    [SerializeField]
+    Sprite ach6;
+    [SerializeField]
+    Sprite ach7;
+    [SerializeField]
+    Sprite ach8;
+    [SerializeField]
+    Sprite ach9;
+    [SerializeField]
+    Sprite ach10;
+
+
+    public static List<Achivement> AchivementsList;
+    private void Start() {
+        AchivementsList.Add(
+            new Achivement(
+                "Collect Wood",
+                "Break trees to get wood",
+                new Dictionary<int, int>(){ { ItemsIndex.getItem(1).itemID, 20 } },
+                ach0
+                ));
+    }
+}
