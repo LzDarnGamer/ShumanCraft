b78a9c17f7a204c4aab9918a24ae9a4c835c8b76
FableArman542 <48763177+FableArman542@users.noreply.github.com>
2020-05-12 22:32:08 +0100

    cenas de online instaciar

--- Assets/Resources/Player/Scripts/ConstructionController.cs
+++ Assets/Resources/Player/Scripts/ConstructionController.cs
@@ -1,53 +1,46 @@
-﻿using System;
+﻿using Photon.Pun;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class ConstructionController : MonoBehaviour {
 
-    [SerializeField]
-    private GameObject[] objects;
-    public int objectIndex = 0;
+    [SerializeField] private GameObject objects;
 
-    [SerializeField]
-    private GameObject placeableObjectPrefab;
+    [SerializeField] private GameObject placeableObjectPrefab;
 
-    [SerializeField]
-    private KeyCode newObjectHotKey = KeyCode.R;
+    [SerializeField] private KeyCode newObjectHotKey = KeyCode.R;
+
+    private bool isOnline = true;
+
+    private Camera cam = null;
 
     private GameObject currentPlaceableObject;
 
     private float angle = 0f;
-    private float angleInc = 5f; 
+    private float angleInc = 2.2f; 
 
     void Update() {
-        placeableObjectPrefab = objects[objectIndex];
-        HandleNewObjectHotKey();
+        if (cam != null) {
+            placeableObjectPrefab = objects;
+            HandleNewObjectHotKey();
 
-        if (currentPlaceableObject != null) {
-            MoveCurrentPlaceableObjectToMouse();
-            RotateFromMouseWheel();
-            ReleaseIfClicked();
+            if (currentPlaceableObject != null) {
+                MoveCurrentPlaceableObjectToMouse();
+                RotateFromMouseWheel();
+                ReleaseIfClicked();
+            }
         }
-
     }
 
-    public void SetHotKey(KeyCode key) {
-        this.newObjectHotKey = key;
-    }
+    public void setOnline(bool y) { isOnline = y; }
 
-    public void SetObjects(GameObject[] obj) {
-        this.objects = obj;
-    }
+    public void setCamera(Camera cam) { this.cam = cam; }
 
-    public void increaseIndex() {
-        objectIndex = (objectIndex + 1) % objects.Length;
-    }
-
-    private void ReleaseIfClicked()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
+    private void ReleaseIfClicked() {
+        if (Input.GetMouseButtonDown(0)) {
             SetColliders(true);
             currentPlaceableObject = null;
         }
@@ -67,30 +60,29 @@ public class ConstructionController : MonoBehaviour {
 
     private void MoveCurrentPlaceableObjectToMouse()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
         RaycastHit hitInfo;
-
+        Debug.DrawRay(cam.transform.position, ray.direction, Color.green);
         if (Physics.Raycast(ray, out hitInfo)) {
-
             if (hitInfo.collider.gameObject.tag.Equals("Terrain")) {
+                Debug.Log("CONSTRUCTION ~ Hit Info: " + hitInfo.point);
                 currentPlaceableObject.transform.position = hitInfo.point;
                 currentPlaceableObject.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
-            }
+            } else Debug.Log("CONSTRUCTION ~ NO TERRAIN");
+        } else {
+            Debug.Log("CONSTRUCTION ~ NO RAYCAST");
         }
     }
 
-    private void HandleNewObjectHotKey()
-    {
-        if (Input.GetKeyDown(newObjectHotKey))
-        {
-            if (currentPlaceableObject == null)
-            {
-                currentPlaceableObject = Instantiate(placeableObjectPrefab);
+    private void HandleNewObjectHotKey() {
+        if (Input.GetKeyDown(newObjectHotKey)) {
+            if (currentPlaceableObject == null) {
+
+                currentPlaceableObject = !isOnline ? Instantiate(placeableObjectPrefab) :
+                    PhotonNetwork.Instantiate(Path.Combine("Scriptable Objects\\Items\\Prefabs\\Placeables", placeableObjectPrefab.name), transform.position, Quaternion.identity, 0);
+                
                 SetColliders(false);
-            }
-            else
-            {
+            } else {
                 Destroy(currentPlaceableObject);
             }
         }
